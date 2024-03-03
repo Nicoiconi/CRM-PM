c296a97e32962ee411b7fad4d99da08694840d53
Nicolas <nicolas.iconicoff@gmail.com>
2024-03-03 19:46:31 -0300

    add clerk validation to home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
-import MainDashboard from "./components/MainDashboard/MainDashboard";
+import { SignedOut, auth } from "@clerk/nextjs"
+import MainDashboard from "./components/MainDashboard/MainDashboard"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function Home() {
+
+  const { userId } = auth()
+  // console.log(userId, user, session)
+
   return (
-    <main className="flex h-screen flex-col justify-center">
-      <MainDashboard />
+    <main className="flex h-screen justify-center">
+      {
+        !userId
+          ? <div className="text-[30px]">
+            Welcome! Please log in.
+
+            <SignedOut>
+              <Button asChild className="button bg-cover">
+                <Link href="/sign-in">
+                  Login
+                </Link>
+              </Button>
+            </SignedOut>
+          </div>
+          : <MainDashboard />
+      }
     </main>
-  );
+  )
 }
--- app/(root)/profile/layout.tsx
+++ app/(root)/profile/layout.tsx
@@ -1,13 +1,5 @@
-import { auth } from '@clerk/nextjs'
-import { redirect } from 'next/navigation'
-import React from 'react'
 
 export default function ProfileLayout({ children }: { children: React.ReactNode }) {
-
-  const { userId, user, session } = auth()
-  // console.log(userId, user, session)
-
-  if (!userId) redirect("/sign-in")
 
   return (
     <div>
