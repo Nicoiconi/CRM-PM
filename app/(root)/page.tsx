484d7246ea87465aa8dfb6c6266af570fbe05047
Nicolas <nicolas.iconicoff@gmail.com>
2024-03-03 18:07:10 -0300

    add MainDashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
+import MainDashboard from "./components/MainDashboard/MainDashboard";
 
 export default function Home() {
   return (
     <main className="flex h-screen flex-col justify-center">
-
+      <MainDashboard />
     </main>
   );
 }
