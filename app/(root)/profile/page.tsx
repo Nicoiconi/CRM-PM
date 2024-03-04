2e913564f234ffbc8d04200fd729c975eea7382a
Nicolas <nicolas.iconicoff@gmail.com>
2024-03-04 00:02:54 -0300

    loggedInUser data

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 
-import { getUserBy } from "@/lib/actions/user.actions"
+import { getUserByClerkId } from "@/lib/actions/user.actions"
 import { resetProperyValues } from "@/lib/database/props/resetPropertyValues"
-import { auth } from "@clerk/nextjs"
-import { redirect } from "next/navigation"
+import { setFooterMessage } from "@/lib/redux/slices/footerSlice/footerSlice"
+import { setLoggedInUser } from "@/lib/redux/slices/usuariosSlice/usuariosSlice"
+// import { UserProfile } from "@clerk/nextjs"
 import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 
 interface PropToReset {
   schema: string
@@ -14,13 +16,15 @@ interface PropToReset {
 
 export default function ProfilePage() {
 
+  const dispatch = useDispatch()
+
+  const { loggedInUser }: { loggedInUser: User } = useSelector((state: Store) => state.users)
+  console.log(loggedInUser)
+
   const [propToReset, setPropToReset] = useState<PropToReset>({
     schema: "",
     propertyName: ""
   })
-
-  // const user = await getUserBy({clerkId: userId})
-  // const images = await getUserImages({ page, userId: user._id })
 
   function handlePropToResetData(e: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = e.target
@@ -35,61 +39,86 @@ export default function ProfilePage() {
     resetProperyValues(propToReset)
   }
 
+  async function handleFetchUserData() {
+    const fetchUser = await getUserByClerkId()
+    if (fetchUser) {
+      const { message, status, object } = fetchUser
+      if (status === 200) {
+        dispatch(setLoggedInUser(object))
+        dispatch(setFooterMessage({ message, status }))
+        return
+      }
+      dispatch(setFooterMessage({ message, status }))
+    }
+  }
+
   return (
     <div>
       <div>
         <button
-          onClick={handleResetProp}
-          disabled={
-            !propToReset?.schema ||
-            !propToReset.propertyName
-          }
+          onClick={handleFetchUserData}
         >
-          Reset Prop
+          Get User data
         </button>
       </div>
 
       <div>
-        <label
-          htmlFor=""
-        >
-          Schema
-        </label>
-        <input
-          type="text"
-          name="schema"
-          value={propToReset?.schema || ""}
-          placeholder="Schema"
-          onChange={(e) => handlePropToResetData(e)}
-        />
-      </div>
+        <div>
+          <button
+            onClick={handleResetProp}
+            disabled={
+              !propToReset?.schema ||
+              !propToReset.propertyName
+            }
+          >
+            Reset Prop
+          </button>
+        </div>
 
-      <div>
-        <label
-          htmlFor=""
-        >
-          Prop
-        </label>
-        <input
-          type="text"
-          name="propertyName"
-          value={propToReset?.propertyName || ""}
-          placeholder="Prop"
-          onChange={(e) => handlePropToResetData(e)}
-        />
-      </div>
+        <div>
+          <label
+            htmlFor=""
+          >
+            Schema
+          </label>
+          <input
+            type="text"
+            name="schema"
+            value={propToReset?.schema || ""}
+            placeholder="Schema"
+            onChange={(e) => handlePropToResetData(e)}
+          />
+        </div>
 
-      <div>
-        <label
-          htmlFor=""
-        >
-          New value
-        </label>
-        {/* <input
+        <div>
+          <label
+            htmlFor=""
+          >
+            Prop
+          </label>
+          <input
+            type="text"
+            name="propertyName"
+            value={propToReset?.propertyName || ""}
+            placeholder="Prop"
+            onChange={(e) => handlePropToResetData(e)}
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor=""
+          >
+            New value
+          </label>
+          {/* <input
           type="text"
           placeholder="New Value"
         /> */}
+        </div>
       </div>
+
+      {/* <UserProfile /> */}
     </div>
   )
 }
