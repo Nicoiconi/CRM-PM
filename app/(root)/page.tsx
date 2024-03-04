2031aa47725610050041142a106d5154f2faafe2
Nicolas <nicolas.iconicoff@gmail.com>
2024-03-03 23:59:18 -0300

    LandingPage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,32 @@
-import { SignedOut, auth } from "@clerk/nextjs"
+"use client"
+
+import Link from "next/link"
+import { SignedIn, SignedOut } from "@clerk/nextjs"
 import MainDashboard from "./components/MainDashboard/MainDashboard"
 import { Button } from "@/components/ui/button"
-import Link from "next/link"
 
 export default function Home() {
 
-  const { userId } = auth()
-  // console.log(userId, user, session)
-
   return (
     <main className="flex h-screen justify-center">
-      {
-        !userId
-          ? <div className="text-[30px]">
-            Welcome! Please log in.
 
-            <SignedOut>
-              <Button asChild className="button bg-cover">
-                <Link href="/sign-in">
-                  Login
-                </Link>
-              </Button>
-            </SignedOut>
+      <SignedOut>
+        <div>
+          <div className="text-[30px]">
+            Welcome! Please log in.
           </div>
-          : <MainDashboard />
-      }
+          <Button asChild className="button bg-cover">
+
+            <Link href="/sign-in">
+              Login
+            </Link>
+          </Button>
+        </div>
+      </SignedOut>
+
+      <SignedIn>
+        <MainDashboard />
+      </SignedIn>
     </main>
   )
 }
